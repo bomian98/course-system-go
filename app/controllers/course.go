8d345776e72b762409a0f33021bd9c69c1554d61
wsczh <1998wsc@gmail.com>
2022-02-17 17:03:48 +0800

    将排课器单拿出来，同时增加初始化，防止多次访问导致数据错误

--- app/controllers/course.go
+++ app/controllers/course.go
@@ -85,40 +85,9 @@ func MakeSchedule(c *gin.Context) {
 		response.Code = common.ParamInvalid
 		response.Data = *new(map[string]string)
 	} else {
-		Map_teacher = request.TeacherCourseRelationShip
-		KM()
-		c.JSON(http.StatusOK, gin.H{"Code": 0, "Data": p})
+		response.Code = common.OK
+		response.Data = services.KM(request.TeacherCourseRelationShip)
 	}
 	c.JSON(http.StatusOK, response)
 	return
-}
-
-var Map_teacher = map[string][]string{}
-var Map_course = map[string][]string{}
-var vis = map[string]bool{} //记录课程是否已被访问过
-var p = map[string]string{} //记录当前课程被哪位老师选中
-
-func match(teacherID string) bool {
-	for _, courseID := range Map_teacher[teacherID] {
-		if !vis[courseID] { //有边且未访问
-			vis[courseID] = true //记录状态未访问过
-			_, ok := p[courseID]
-			if !ok || match(p[courseID]) { //如果暂无匹配，或者原来匹配的左侧元素可以找到新的匹配
-				p[courseID] = teacherID //当前左侧元素成为当前右侧元素的新匹配
-				return true             //返回匹配成功
-			}
-		}
-	}
-	return false
-}
-
-func KM() {
-	for teacherID, courseIDs := range Map_teacher {
-		for _, courseID := range courseIDs {
-			Map_course[teacherID] = append(Map_course[teacherID], courseID)
-		}
-	}
-	for teacherID, _ := range Map_teacher {
-		match(teacherID)
-	}
 }
---
+++ app/services/schedule_course.go
@@ -0,0 +1,41 @@
+package services
+
+var MapTeacher = map[string][]string{}
+var MapCourse = map[string][]string{}
+var vis = map[string]bool{} //记录课程是否已被访问过
+var p = map[string]string{} //记录当前课程被哪位老师选中
+
+func initSchedule() {
+	MapTeacher = map[string][]string{}
+	MapCourse = map[string][]string{}
+	vis = map[string]bool{} //记录课程是否已被访问过
+	p = map[string]string{} //记录当前课程被哪位老师选中
+}
+
+func match(teacherID string) bool {
+	for _, courseID := range MapTeacher[teacherID] {
+		if !vis[courseID] { //有边且未访问
+			vis[courseID] = true //记录状态未访问过
+			_, ok := p[courseID]
+			if !ok || match(p[courseID]) { //如果暂无匹配，或者原来匹配的左侧元素可以找到新的匹配
+				p[courseID] = teacherID //当前左侧元素成为当前右侧元素的新匹配
+				return true             //返回匹配成功
+			}
+		}
+	}
+	return false
+}
+
+func KM(mapTeacher map[string][]string) map[string]string {
+	initSchedule()
+	MapTeacher = mapTeacher
+	for teacherID, courseIDs := range MapTeacher {
+		for _, courseID := range courseIDs {
+			MapCourse[teacherID] = append(MapCourse[teacherID], courseID)
+		}
+	}
+	for teacherID, _ := range MapTeacher {
+		match(teacherID)
+	}
+	return p
+}
